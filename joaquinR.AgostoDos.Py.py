cc36289f8150487531a9b390c9dc4f83ef9f21f8
JoaquinRomero <163877987+JoaquinRomero7429@users.noreply.github.com>
2024-08-12 12:18:17 -0300

    Add files via upload
    Trate de modificar un archivo que ya tenia

---
+++ joaquinR.AgostoDos.Py.py
@@ -0,0 +1,21 @@
+def añadirTarea()
+    tareas[]= 
+
+    print ("Tarea agregada")
+def mostrarTareas (tarea,nombre)
+    if nombre in contacto:
+        print(f"tareas: , telefono: {contacto[nombre]}")
+    else:
+        print ("error al encontrar el contacto")
+def listaDeTareas(contactos):
+    if contactos
+    print ("lista de contactos")
+    for nombre , numDeTelefono in contactos.i():
+        print (f"Nombre: {nombre}, telefono: {numDeTelefono}")
+
+    else:
+        print("no hay tareasguardadas ")
+
+def borrarContacto(contactos, nombre)
+    if nombre in contactos:
+        del contactos
