40a73909bc565a8ebf8e456abc7a5b48ab9b50b1
JoaquinRomero <163877987+JoaquinRomero7429@users.noreply.github.com>
2024-10-02 10:51:25 -0300

    Add files via upload

--- joaquinR.AgostoDos.Py.py
+++ joaquinR.AgostoDos.Py.py
@@ -1,21 +1,44 @@
-def añadirTarea()
-    tareas[]= 
+tareas = []
 
-    print ("Tarea agregada")
-def mostrarTareas (tarea,nombre)
-    if nombre in contacto:
-        print(f"tareas: , telefono: {contacto[nombre]}")
-    else:
-        print ("error al encontrar el contacto")
-def listaDeTareas(contactos):
-    if contactos
-    print ("lista de contactos")
-    for nombre , numDeTelefono in contactos.i():
-        print (f"Nombre: {nombre}, telefono: {numDeTelefono}")
+while True:
+    print("****************************")
+    print("        MENÚ DE OPCIONES     ")
+    print("****************************")
+    print("1: Añadir tarea")
+    print("2: Ver tareas")
+    print("3: Completar tarea")
+    print("4: Salir")
+    print("****************************")
+    
+    opcion = input("Elige una opción: ")
 
-    else:
-        print("no hay tareasguardadas ")
+    if opcion == "1":
+        tarea = input("Descripción de la tarea: ")
+        tareas.append(tarea)
 
-def borrarContacto(contactos, nombre)
-    if nombre in contactos:
-        del contactos
+    elif opcion == "2":
+        if tareas:
+            for i in range(1, 999):
+                if i <= len(tareas):  
+                    print(f"{i}. {tareas[i - 1]}")
+                else:
+                    break
+        else:
+            print("No hay tareas.")
+
+    elif opcion == "3":
+        if tareas:
+            num = int(input("Número de tarea completada: ")) - 1
+            if num >= 0 and num < 999:  
+                if num < len(tareas):  
+                    tareas.pop(num)  
+                else:
+                    print("Número inválido.")
+            else:
+                print("Número inválido.")
+        else:
+            print("No hay tareas para completar.")
+
+    elif opcion == "4":
+        print("Gracias por utilizar este programa")
+        break
